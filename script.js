383e1047d8aea1df8b452b6743725aa1745a41fb
panteraink <panteraink@gmail.com>
2022-03-24 17:25:37 +0800

    Rewrited script, everything works

--- script.js
+++ script.js
@@ -1,37 +1,22 @@
 "use strict";
 
-let expandingBtnSiblingsArray = [];
-
-//Анимация разворачиваемой кнопки
-const expandingBtnClick = (obj) => {
-    let bgColor = obj.style.backgroundColor
-    if (obj.dataset.expandingButton === "closed") {
-        obj.style.zIndex = '100';
-        obj.classList.toggle('expanded');
-        obj.dataset.expandingButton = "expanded";
-    } else if (obj.dataset.expandingButton === "expanded") {
-        obj.classList.toggle('expanded');
-        setTimeout(() => {
-            obj.style.zIndex = ''
-        }, 500);
-        obj.dataset.expandingButton = "closed"
+//Получаем внутренний конейнер и вешаем на него листенер
+let container = document.querySelector('.inner-wrapper');
+container.addEventListener('click', (event) => {
+    if (event.target.closest('.button-rotating')) {
+        event.target.closest('.button-rotating').classList.toggle('rotated');
+    } else if (event.target.closest('.button-expanding')) {
+        const obj = event.target.closest('.button-expanding');
+        if (obj.dataset.expandingButton === "closed") {
+            obj.style.zIndex = '100';
+            obj.classList.toggle('expanded');
+            obj.dataset.expandingButton = "expanded";
+        } else if (obj.dataset.expandingButton === "expanded") {
+            obj.classList.toggle('expanded');
+            setTimeout(() => {
+                obj.style.zIndex = ''
+            }, 500);
+            obj.dataset.expandingButton = "closed"
+        }
     }
-}; 
-
-//Получаем все кнопки и преобразуем в массив
-let rotatingBtnsArray = document.querySelectorAll('[data-button-rotating]');
-let expandingBtnsArray = document.querySelectorAll('[data-expanding-button]');
-
-//Тоггл по клику
-for (const elem of rotatingBtnsArray){
-    elem.addEventListener('click', () => {
-        return elem.classList.toggle('rotated');
-    });
-};
-
-for (const elem of expandingBtnsArray){
-    elem.dataset.expandingButton = "closed";
-    elem.addEventListener('click', () => {
-        return expandingBtnClick(elem);
-    });
-};
+});
