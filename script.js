c5705328fea1b3d58d28c474fb4df4624568028a
panteraink <panteraink@gmail.com>
2022-04-12 21:38:32 +0800

    Miscellanious

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 import './js/slider';
 import './js/expandingBtns'
 
-if (window.matchMedia("(any-hover: none)").matches) {
+if (window.matchMedia("(any-hover: none)").matches){
     const nameBlock = document.querySelector('.name');
     const nameImage = nameBlock.querySelector('.name_image');
     const imageUp = () => {
-        nameImage.classList.toggle('name_image_up')
+        nameImage.classList.toggle('name_image_up');
     };
     nameBlock.addEventListener('click', imageUp);
 };
